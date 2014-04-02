def2bddc31195d503908a1153f2e6f6b13acf614
Clare Maloney-McCrystle <claretheawesome@Clare-Maloney-McCrystles-MacBook.local>
2014-04-02 09:00:36 -0700

    Initial Commit

---
+++ MakeMeASandwhich/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MakeMeASandwhich
+//
+//  Created by Clare Maloney-McCrystle on 4/2/14.
+//  Copyright (c) 2014 Clare Maloney-McCrystle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MakeMeASandwhich/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MakeMeASandwhich
+//
+//  Created by Clare Maloney-McCrystle on 4/2/14.
+//  Copyright (c) 2014 Clare Maloney-McCrystle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
